a57f83237e10f8dfb40547215b53e478c5638032
Saurav Pant <sauravpant777@gmail.com>
2025-10-30 22:27:44 +0545

    fix: improve logger output format

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -3,18 +3,14 @@ import winston from "winston";
 const environment = process.env.NODE_ENV;
 
 const customFormat = winston.format.printf(({ level, message, timestamp }) => {
-  return `{ ${timestamp}  [${level.toUpperCase()}]: ${message} }`;
+  return `{ ${timestamp}  [${level.toUpperCase()}] -> ${message} }`;
 });
 
 const logger = winston.createLogger({
   level: environment === "production" ? "debug" : "info",
-  format: winston.format.combine(
-    winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
-    customFormat,
-    winston.format.colorize({ all: true }) // Then apply custom format after color
-  ),
+  format: winston.format.combine(winston.format.timestamp({ format: "YYYY-MM-DD HH:mm:ss" }), customFormat, winston.format.colorize({ all: true })),
   transports: [
-    new winston.transports.Console(), // Console transport uses the combined format
+    new winston.transports.Console(),
     new winston.transports.File({ filename: "logs/error.log", level: "error" }),
     new winston.transports.File({ filename: "logs/combined.log" }),
   ],
