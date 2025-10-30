47c61b7856d60d394cdf5c04b0679602b722af42
Saurav Pant <sauravpant777@gmail.com>
2025-10-31 00:26:13 +0545

    feat: enhance BullMQ Redis client configuration

--- server/src/configs/bullmq.ts
+++ server/src/configs/bullmq.ts
@@ -1,3 +1,14 @@
 import IORedis from "ioredis";
+import logger from "../utils/logger";
 
-export const bullClient = new IORedis(process.env.REDIS_URL);
+export const bullClient = new IORedis(process.env.REDIS_URL,{
+  maxRetriesPerRequest: null,
+});
+
+bullClient.on("connect", () => {
+  logger.info("Successfully connected to Redis");
+});
+
+bullClient.on("error", (err) => {
+  logger.error(`Redis connection error: ${err.message}`);
+});
