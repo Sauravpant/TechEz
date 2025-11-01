63550f4884dde2aa7518dc0e908848eac29e3c8a
Saurav Pant <sauravpant777@gmail.com>
2025-11-02 00:11:17 +0545

    feat: add user profile routes

---
+++ server/src/routes/user/user-profile.routes.ts
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import { verifyJWT } from "../../middlewares/auth.middleware";
+import { deleteUserProfilePicture, getUserProfile, updateUserProfile, uploadUserProfilePicture } from "../../controllers/user/user-profile.controller";
+import { upload } from "../../middlewares/multer.middleware";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.patch("/update-profile", updateUserProfile);
+router.patch("/upload-profile-picture", upload.single("profilePicture"), uploadUserProfilePicture);
+router.get("/profile", getUserProfile);
+router.delete("/delete-profile-picture", deleteUserProfilePicture);
+
+export default router;
