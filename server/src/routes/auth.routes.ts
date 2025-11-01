ebb5e15339a85e24882f4e9e8e5752165a798b85
Saurav Pant <sauravpant777@gmail.com>
2025-11-01 23:55:12 +0545

    feat: add deactivate account route to auth routes

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { changePassword, forgotPassword, login, logout, registerTechnician, registerUser, sendOtp } from "../controllers/auth.controller";
+import { changePassword, deactivateAccount, forgotPassword, login, logout, registerTechnician, registerUser, sendOtp } from "../controllers/auth.controller";
 import { verifyJWT } from "../middlewares/auth.middleware";
 
 const router = Router();
@@ -11,5 +11,6 @@ router.post("/logout", verifyJWT, logout);
 router.post("/send-otp", sendOtp);
 router.patch("/forgot-password", forgotPassword);
 router.patch("/change-password", verifyJWT,changePassword);
+router.patch("/deactivate-account", verifyJWT,deactivateAccount);
 
 export default router;
