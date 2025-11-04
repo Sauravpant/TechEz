acb4c8bc5075f08c7eb1391fcdce735fc41bd725
Saurav Pant <sauravpant777@gmail.com>
2025-11-04 20:09:00 +0545

    feat: add technician profile update and document upload/delete functionalities

--- server/src/controllers/technician.controller.ts
+++ server/src/controllers/technician.controller.ts
@@ -1,12 +1,47 @@
 import { Request, Response } from "express";
 import { asyncHandler } from "../utils/async-handler";
-import { getAllTechniciansSchema } from "../validators/technician.validators";
-import { getAllTechniciansService } from "../services/technician.services";
+import { getAllTechniciansSchema, updateTechnicianSchema } from "../validators/technician.validators";
+import {
+  deleteVerificationDocumentService,
+  getAllTechniciansService,
+  getTechnicianByIdService,
+  updateTechnicianProfileService,
+  uploadVerificationDocumentService,
+} from "../services/technician.services";
 import { ApiResponse } from "../utils/api-response";
+import { AuthenticatedRequest } from "../types/auth.types";
+import { AppError } from "../utils/app-error";
 
 export const getAllTechnicians = asyncHandler(async (req: Request, res: Response) => {
   const query = getAllTechniciansSchema.parse(req.query);
   const result = await getAllTechniciansService(query);
-
   return res.status(200).json(new ApiResponse(200, result, "Technicians fetched successfully"));
+});
+
+export const getTechnicianById = asyncHandler(async (req: Request, res: Response) => {
+  const { technicianId } = req.params;
+  const result = await getTechnicianByIdService(technicianId);
+  return res.status(200).json(new ApiResponse(200, result, "Technician details fetched successfully"));
+});
+
+export const updateTechnicianDetails = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+  const technicianId = req.user.id;
+  const data = updateTechnicianSchema.parse(req.body);
+  const result = await updateTechnicianProfileService(technicianId, data);
+  return res.status(200).json(new ApiResponse(200, result, "Technician profile updated successfully"));
+});
+
+export const uploadVerificationDocument = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+  const file = req.file;
+  if (!file) {
+    throw new AppError(400, "Document is required");
+  }
+  const result = await uploadVerificationDocumentService(req.user._id.toString(), { fileBuffer: file.buffer, fileName: file.originalname });
+  return res.status(200).json(new ApiResponse(200, result, "Verification document uploaded successfully"));
+});
+
+
+export const deleteVerificationDocument = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+  await deleteVerificationDocumentService(req.user._id.toString());
+  return res.status(200).json(new ApiResponse(200, null, "Verification document deleted successfully"));
 });
--- server/src/services/technician.services.ts
+++ server/src/services/technician.services.ts
@@ -1,10 +1,15 @@
 import { Category, ICategory } from "../models/category.model";
-import { Technician } from "../models/technician.model";
-import { IUser } from "../models/user.model";
-import { GetAllTechnicians, GetAllTechniciansFilter } from "../types/technician.types";
+import { ITechnician, Technician } from "../models/technician.model";
+import { IUser, User } from "../models/user.model";
+import { GetAllTechnicians, GetAllTechniciansFilter, TechnicianDetails, UpdateTechnician } from "../types/technician.types";
 import { redisClient } from "../configs/redis";
+import { AppError } from "../utils/app-error";
+import { Booking } from "../models/booking.model";
+import { Review } from "../models/review.model";
+import { TechnicianInfo } from "../types/auth.types";
+import { deleteFromCloudinary, uploadToCloudinary } from "../utils/cloudinary";
 
-export const getAllTechniciansService = async (filter: GetAllTechniciansFilter): Promise<GetAllTechnicians[]> => {
+export const getAllTechniciansService = async (filter: GetAllTechniciansFilter): Promise<GetAllTechnicians> => {
   const limit = filter.limit ?? 10;
   const page = filter.page ?? 1;
   const skip = (page - 1) * limit;
@@ -13,12 +18,12 @@ export const getAllTechniciansService = async (filter: GetAllTechniciansFilter):
   const cachedData = await redisClient.get(cacheKey);
   if (cachedData) {
     const cachedString = typeof cachedData === "string" ? cachedData : cachedData.toString();
-    return JSON.parse(cachedString) as GetAllTechnicians[];
+    return JSON.parse(cachedString) as GetAllTechnicians;
   }
   const filters: Record<string, any> = {};
   if (filter.category) {
     const categoryExists = await Category.findOne({ name: filter.category });
-    if (!categoryExists) return [];
+    if (!categoryExists) return { technicians: [], total: 0, page, limit, totalPages: 0 };
     filters.category = categoryExists._id;
   }
 
@@ -38,6 +43,8 @@ export const getAllTechniciansService = async (filter: GetAllTechniciansFilter):
     .skip(skip)
     .limit(limit)
     .lean();
+  const total = await Technician.countDocuments(filters);
+  const totalPages = Math.ceil(total / limit);
   const result = technicians.map((t) => ({
     id: t._id.toString(),
     name: t.user.name,
@@ -51,9 +58,203 @@ export const getAllTechniciansService = async (filter: GetAllTechniciansFilter):
     registeredAt: t.createdAt,
   }));
 
+  const response: GetAllTechnicians = {
+    technicians: result,
+    total,
+    page,
+    limit,
+    totalPages,
+  };
   //Set the data in  redis cache for 5 minutes for subsequent requests
-  await redisClient.set(cacheKey, JSON.stringify(result), { EX: 300 });
+  await redisClient.set(cacheKey, JSON.stringify(response), { EX: 300 });
 
-  return result;
+  return response;
 };
 
+export const getTechnicianByIdService = async (id: string): Promise<TechnicianDetails> => {
+  const cacheKey = `technician:${id}`;
+  const cachedData = await redisClient.get(cacheKey);
+  if (cachedData) {
+    const cachedResponse = typeof cachedData === "string" ? cachedData : cachedData.toString();
+    return JSON.parse(cachedResponse) as TechnicianDetails;
+  }
+  const technician = await Technician.findById(id)
+    .populate<{ user: IUser }>({
+      path: "user",
+      select: "name address isVerified profilePictureUrl",
+    })
+    .populate<{ category: ICategory }>("category", "name")
+    .lean();
+
+  if (!technician) throw new AppError(404, "Technician not found");
+
+  const totalBookings = await Booking.countDocuments({
+    technician: technician._id,
+    status: "completed",
+  });
+  const reviewsData = await Review.aggregate([
+    { $match: { technician: technician._id } },
+    {
+      $lookup: {
+        from: "users",
+        localField: "user",
+        foreignField: "_id",
+        as: "userInfo",
+      },
+    },
+    { $unwind: "$userInfo" },
+    {
+      $group: {
+        _id: "$technician",
+        averageRating: { $avg: "$rating" },
+        totalReviews: { $sum: 1 },
+        reviews: {
+          $push: {
+            id: "$_id",
+            rating: "$rating",
+            comment: "$comment",
+            createdAt: "$createdAt",
+            user: {
+              id: "$userInfo._id",
+              name: "$userInfo.name",
+              profilePictureUrl: "$userInfo.profilePictureUrl",
+            },
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        _id: 0,
+        averageRating: { $ifNull: [{ $round: ["$averageRating", 1] }, 0] },
+        totalReviews: { $ifNull: ["$totalReviews", 0] },
+        reviews: { $ifNull: ["$reviews", []] },
+      },
+    },
+  ]);
+
+  const reviewSummary = reviewsData[0] ?? {
+    averageRating: 0,
+    totalReviews: 0,
+    reviews: [],
+  };
+  console.log(reviewsData[0]);
+
+  const response: TechnicianDetails = {
+    id: technician._id.toString(),
+    name: technician.user.name,
+    address: technician.user.address,
+    isVerified: technician.user.isVerified,
+    profilePictureUrl: technician.user.profilePictureUrl,
+    experience: technician.experience,
+    category: technician.category?.name,
+    bio: technician.bio,
+    description: technician.description,
+    registeredAt: technician.createdAt,
+    totalBookings,
+    averageRating: reviewSummary.averageRating,
+    totalReviews: reviewSummary.totalReviews,
+    reviews: reviewSummary.reviews,
+  };
+
+  //Set the data in  redis cache for 3 minutes for subsequent requests
+  await redisClient.set(cacheKey, JSON.stringify(response), { EX: 180 });
+  return response;
+};
+
+export const updateTechnicianProfileService = async (id: string, data: UpdateTechnician): Promise<TechnicianInfo> => {
+  const userData: Partial<IUser> = {};
+  if (data.name !== undefined) userData.name = data.name;
+  if (data.address !== undefined) userData.address = data.address;
+  if (data.phone !== undefined) userData.phone = data.phone;
+
+  const technicianData: Partial<ITechnician> = {};
+  if (data.experience !== undefined) technicianData.experience = data.experience;
+  if (data.category !== undefined) {
+    const categoryExists = await Category.findOne({ name: data.category });
+    if (!categoryExists) {
+      throw new AppError(400, "Invalid category");
+    }
+    technicianData.category = categoryExists._id;
+  }
+  if (data.bio !== undefined) technicianData.bio = data.bio;
+  if (data.description !== undefined) technicianData.description = data.description;
+  const technician = await Technician.findOne({ user: id });
+  if (!technician) {
+    throw new AppError(404, "Technician not found");
+  }
+  const [userInfo, technicianInfo] = await Promise.all([
+    User.findByIdAndUpdate(id, userData, { new: true }),
+    Technician.findByIdAndUpdate(technician._id, technicianData, { new: true }).populate<{ category: ICategory }>("category", "name"),
+  ]);
+
+  await redisClient.del(`technician:${id}`);
+  for await (const key of redisClient.scanIterator({ MATCH: "technicians:*" })) {
+    if (key.length > 0) {
+      await redisClient.del(key);
+    }
+  }
+  const user = {
+    id: userInfo._id.toString(),
+    name: userInfo.name,
+    email: userInfo.email,
+    phone: userInfo.phone,
+    profilePictureUrl: userInfo.profilePictureUrl,
+    address: userInfo.address,
+    role: userInfo.role,
+    isVerified: userInfo.isVerified,
+    isDeactivated: userInfo.isDeactivated,
+    createdAt: userInfo.createdAt,
+    updatedAt: userInfo.updatedAt,
+  };
+  const response: TechnicianInfo = {
+    ...user,
+    technicianId: technicianInfo?._id.toString(),
+    user: technicianInfo.user.toString(),
+    category: technicianInfo.category.name,
+    experience: technicianInfo.experience,
+    bio: technicianInfo.bio,
+    description: technicianInfo.description,
+    registrationNumber: technicianInfo.registrationNumber,
+    documentUrl: technicianInfo.documentUrl,
+    status: technicianInfo.status,
+  };
+
+  return response;
+};
+
+export const uploadVerificationDocumentService = async (userId: string, file: { fileBuffer: Buffer; fileName: string }): Promise<string> => {
+  const technician = await Technician.findOne({ user: userId });
+  if (!technician) {
+    throw new AppError(404, "Technician not found");
+  }
+  let imageUrl: string = null;
+  if (!technician.documentPublicId) {
+    const uploadResult = await uploadToCloudinary(file.fileBuffer, file.fileName);
+    technician.documentPublicId = uploadResult.public_id;
+    technician.documentUrl = uploadResult.secure_url;
+    imageUrl = uploadResult.secure_url;
+    await technician.save();
+  } else {
+    await deleteFromCloudinary(technician.documentPublicId);
+    const uploadResult = await uploadToCloudinary(file.fileBuffer, file.fileName);
+    technician.documentPublicId = uploadResult.public_id;
+    technician.documentUrl = uploadResult.secure_url;
+    imageUrl = uploadResult.secure_url;
+    await technician.save();
+  }
+  return imageUrl;
+};
+
+export const deleteVerificationDocumentService = async (userId: string): Promise<void> => {
+  const technician = await Technician.findOne({ user: userId });
+  if (!technician) {
+    throw new AppError(404, "Technician not found");
+  }
+  if (technician.documentPublicId) {
+    await deleteFromCloudinary(technician.documentPublicId);
+    technician.documentPublicId = null;
+    technician.documentUrl = null;
+    await technician.save();
+  }
+};
