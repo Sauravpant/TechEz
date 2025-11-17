028f6e828fc66bf3fee8203ff3296e521355e652
Saurav Pant <sauravpant777@gmail.com>
2025-11-18 01:02:20 +0545

    feat: add booking routes

---
+++ server/src/routes/booking.routes.ts
@@ -0,0 +1,29 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware";
+import { verifyUser } from "../middlewares/user.middleware";
+import {
+  acceptManualBooking,
+  cancelTechnicianBooking,
+  completeManualBooking,
+  createManualBookingRequest,
+  raiseManualBookingPrice,
+  userAgreement,
+  userCancelBooking,
+} from "../controllers/booking.controller";
+import { verifyTechnician } from "../middlewares/technician.middleware";
+
+const router = Router();
+
+router.use(verifyJWT);
+
+router.post("/create-booking", verifyUser, createManualBookingRequest);
+router.patch("/raise-booking-price", verifyUser, verifyTechnician, raiseManualBookingPrice);
+router.patch("/cancel-technician-booking/:bookingId", verifyUser, verifyTechnician, cancelTechnicianBooking);
+router.patch("/accept-booking/:bookingId", verifyUser, verifyTechnician, acceptManualBooking);
+router.patch("/complete-booking/:bookingId", verifyUser, verifyTechnician, completeManualBooking);
+router.patch("/user-agreement/:bookingId", verifyUser, userAgreement);
+router.patch("/cancel-user-booking/:bookingId", verifyUser, userCancelBooking);
+
+router.use(verifyJWT);
+
+export default router;
