2bf008fbe24d7dfb5a3a7096485a5ceed4478609
Saurav Pant <sauravpant777@gmail.com>
2026-04-10 11:20:36 +0545

    feat: update dependencies and enhance project configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,9 +1,15 @@
+import path from "path";
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   optimizeDeps: {
     exclude: [
       "@radix-ui/react-separator",
