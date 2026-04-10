69b548b2a80aa0fce68d2793fd24e00c192896db
Saurav Pant <sauravpant777@gmail.com>
2026-04-10 11:07:20 +0545

    feat: update store configuration

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "../features/auth/authSlice";
-import verificationReducer from "../features/auth/verificationSlice";
-export const store = configureStore({
+import authReducer from "@/features/auth/authSlice";
+
+const store = configureStore({
   reducer: {
     auth: authReducer,
-    verification: verificationReducer,
   },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
+
